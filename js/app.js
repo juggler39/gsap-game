b6122c693f830d3a181a1edd9e84cea770c3a8cd
juggler39 <juggler39@gmail.com>
2021-07-27 22:42:32 +0300

    end game

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
+    gsap.set('.image', { scale: 0, opacity: 0 });
     initTitle();
-    init();
 });
 
 const titleText = new SplitText('.title');
@@ -10,9 +10,21 @@ const dropTargets = document.querySelectorAll('.target');
 const totalTargets = 6;
 let totalHit = 0;
 
-const tl = gsap.timeline({});
+const tl = gsap.timeline({ onComplete: init });
 
-function init() {}
+function init() {
+    gsap.fromTo(
+        '.image',
+        { scale: 0, opacity: 0 },
+        {
+            scale: 1,
+            opacity: 1,
+            stagger: { amount: 0.5 },
+            duration: 0.5,
+            onComplete: initDraggable,
+        }
+    );
+}
 
 function initTitle() {
     for (let i = 0; i < numWords; i++) {
@@ -27,5 +39,93 @@ function initTitle() {
             },
             Math.random()
         );
+    }
+}
+
+function initDraggable() {
+    Draggable.create('.dragItem', {
+        bounds: gameContainer,
+        edgeResistance: 0.75,
+        type: 'x, y',
+        cursor: 'grab',
+        activeCursor: 'grabbing',
+        onPress: function () {
+            this.startX = this.x;
+            this.startY = this.y;
+            this.offsetTop =
+                this.startY - this.target.getBoundingClientRect().top;
+            this.offsetLeft =
+                this.startX - this.target.getBoundingClientRect().left;
+        },
+        onDragEnd: function () {
+            const dragID = this.target.id + 'Drop';
+            dropTargets.forEach((el) => {
+                const spotId = el.id;
+                const pos = el.getBoundingClientRect();
+                const diffTop = this.offsetTop + pos.top;
+                const diffLeft = this.offsetLeft + pos.left;
+                if (spotId == dragID) {
+                    if (this.hitTest(el, '70%')) {
+                        gsap.to('.correct', {
+                            duration: 0.3,
+                            scale: 2,
+                            autoAlpha: 1,
+                            onComplete: function () {
+                                gsap.to('.correct', {
+                                    duration: 0.3,
+                                    scale: 0,
+                                    autoAlpha: 0,
+                                    delay: 0.5,
+                                });
+                            },
+                        });
+
+                        gsap.to(this.target, {
+                            duration: 0.5,
+                            x: diffLeft,
+                            y: diffTop,
+                            onComplete: hideMatches,
+                            onCompleteParams: [this.target, el],
+                        });
+                    } else {
+                        gsap.to('.tryAgain', {
+                            duration: 0.3,
+                            scale: 2,
+                            autoAlpha: 1,
+                            onComplete: function () {
+                                gsap.to('.tryAgain', {
+                                    duration: 0.3,
+                                    scale: 0,
+                                    autoAlpha: 0,
+                                    delay: 0.5,
+                                });
+                            },
+                        });
+                        gsap.to(this.target, {
+                            duration: 0.5,
+                            x: this.startX,
+                            y: this.startY,
+                        });
+                    }
+                }
+            });
+        },
+    });
+}
+
+function hideMatches(dragItem, targetItem) {
+    totalHit++;
+    gsap.to([dragItem, targetItem], {
+        duration: 0.5,
+        autoAlpha: 0,
+        onComplete: checkTargetCount,
+    });
+}
+function checkTargetCount() {
+    if (totalHit === totalTargets) {
+        gsap.to('.modal', {
+            duration: 0.3,
+            autoAlpha: 1,
+        });
     }
 }
